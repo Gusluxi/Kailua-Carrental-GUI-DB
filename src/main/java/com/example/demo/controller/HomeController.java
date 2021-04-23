636936a228c0ae4699cd2e1c6e6ac6cdc244a05b
Kristian097 <73465821+Kristian097@users.noreply.github.com>
2021-04-23 13:29:41 +0200

    Noget view all cars liste ish, men vi skal lige lave en knap til, at gå til den side

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,12 +1,31 @@
 package com.example.demo.controller;
-
+import com.example.demo.repository.CarRepo;
+import com.example.demo.model.Car;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+    @Autowired
+    CarRepo carRepo;
+
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        List<Car> carList = carRepo.fetchAll();
+        model.addAttribute("cars", carList);
         return "home/index";
     }
+
+    @GetMapping ("/viewCars")
+    public String viewCars(Model model) {
+        List<Car> carList = carRepo.fetchAll();
+        model.addAttribute("cars", carList);
+        return "home/viewCars";
+    }
+
 }
