d4cb3d662c2bef6ede9b5e445bd783fde408763b
FrederikALP <fred05k3@stud.kea.dk>
2021-04-27 09:33:10 +0200

    Revert "Contract klasse"
    This reverts commit 0b8ad870a1720bf89f715baf79588b5daf1cc830.
    
    # Conflicts:
    #	src/main/java/com/example/demo/model/Contract.java

--- src/main/java/com/example/demo/model/Contract.java
+++ src/main/java/com/example/demo/model/Contract.java
@@ -1,20 +1,13 @@
 package com.example.demo.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
 import java.sql.Date;
 
-@Entity
 public class Contract {
 
-    @Id
     private int contractID;
     private Date contractDateStart;
     private int maxKM;
     private int startOdometer;
-    private int customerID;
-    private String plateNumber;
-
-
-
+    private int customerIDContract;
+    private String plateNumberContract;
 }
