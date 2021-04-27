c813129aa2371dab86e30528d371a7d7dbae8c1e
FrederikALP <fred05k3@stud.kea.dk>
2021-04-27 09:40:05 +0200

    Contract klasse

--- src/main/java/com/example/demo/model/Contract.java
+++ src/main/java/com/example/demo/model/Contract.java
@@ -3,19 +3,44 @@ package com.example.demo.model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.sql.Date;
+import java.time.LocalDate;
 
 @Entity
 public class Contract {
 
     @Id
     private int contractID;
-    private Date contractDateStart;
+    private LocalDate contractDateStart;
     private int maxKM;
     private int startOdometer;
     private int customerID;
     private String plateNumber;
 
-    you suck bro...
+    public Contract() {
+    }
 
+    public int getContractID() {return contractID; }
+
+    public void setContractID(int contractID) {this.contractID = contractID; }
+
+    public LocalDate getContractDateStart() {return contractDateStart; }
+
+    public void setContractDateStart(LocalDate contractDateStart) {this.contractDateStart = contractDateStart; }
+
+    public int getMaxKM() {return maxKM; }
+
+    public void setMaxKM(int maxKM) {this.maxKM = maxKM; }
+
+    public int getStartOdometer() {return startOdometer; }
+
+    public void setStartOdometer(int startOdometer) {this.startOdometer = startOdometer; }
+
+    public int getCustomerID() {return customerID; }
+
+    public void setCustomerID(int customerID) {this.customerID = customerID; }
+
+    public String getPlateNumber() {return plateNumber; }
+
+    public void setPlateNumber(String plateNumber) {this.plateNumber = plateNumber; }
 
 }
