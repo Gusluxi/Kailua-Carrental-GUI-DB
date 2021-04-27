c64da3970ddc831e7cc40a05286eb0bd2d1e594a
Gustav <gullegustav@gmail.com>
2021-04-27 09:35:12 +0200

    Repo Classes

--- src/main/java/com/example/demo/Repository/CarRepo.java
+++ src/main/java/com/example/demo/Repository/CarRepo.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @Repository
 public class CarRepo {
+
     @Autowired
     JdbcTemplate template;
 
--- src/main/java/com/example/demo/Repository/CustomerRepo.java
+++ src/main/java/com/example/demo/Repository/CustomerRepo.java
@@ -1,4 +1,23 @@
 package com.example.demo.Repository;
 
+import com.example.demo.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class CustomerRepo {
+
+    @Autowired
+    JdbcTemplate template;
+
+    public List<Customer> fetchAll() {
+        String sql = "SELECT * FROM customers";
+        RowMapper<Customer> rowMapper = new BeanPropertyRowMapper<>(Customer.class);
+        return template.query(sql, rowMapper);
+    }
 }
