0b8ad870a1720bf89f715baf79588b5daf1cc830
FrederikALP <fred05k3@stud.kea.dk>
2021-04-27 09:30:08 +0200

    Contract klasse

--- src/main/java/com/example/demo/model/Contract.java
+++ src/main/java/com/example/demo/model/Contract.java
@@ -1,13 +1,64 @@
 package com.example.demo.model;
 
 import java.sql.Date;
+import java.time.LocalDate;
 
 public class Contract {
 
     private int contractID;
-    private Date contractDateStart;
+    private LocalDate contractDateStart;
     private int maxKM;
     private int startOdometer;
     private int customerIDContract;
     private String plateNumberContract;
+
+    public Contract() {
+    }
+
+    public String getContractID() {
+        return contractID;
+    }
+
+    public void setContractID(int contractID) {
+        this.contractID = contractID;
+    }
+
+    public LocalDate getContractDateStart() {
+        return contractDateStart;
+    }
+
+    public void setContractDateStart(LocalDate contractDateStart) {
+        this.contractDateStart = contractDateStart;
+    }
+
+    public int getMaxKM() {
+        return maxKM;
+    }
+
+    public void setMaxKM(int maxKM) {
+        this.maxKM = maxKM;
+    }
+
+    public int getStartOdometer() {
+        return startOdometer;
+    }
+
+    public void setStartOdometer(int startOdometer) {
+        this.startOdometer = startOdometer;
+    }
+
+    public int getCustomerIDContract() {
+        return customerIDContract;
+    }
+
+    public void setCustomerIDContract(int customerIDContract) {
+        this.customerIDContract = customerIDContract;
+    }
+
+    public String getPlateNumberContract() { return plateNumberContract; }
+
+    public void setPlateNumberContract(String plateNumberContract) {
+        this.plateNumberContract = plateNumberContract;
+    }
+
 }
