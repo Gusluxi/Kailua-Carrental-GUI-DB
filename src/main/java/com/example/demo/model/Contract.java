be1e5faa840e8f618c21ea8d569515b9cbefce40
Gustav <gullegustav@gmail.com>
2021-04-27 09:23:53 +0200

    Empty comiment

--- src/main/java/com/example/demo/model/Contract.java
+++ src/main/java/com/example/demo/model/Contract.java
@@ -1,13 +1,20 @@
 package com.example.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.sql.Date;
 
+@Entity
 public class Contract {
 
+    @Id
     private int contractID;
     private Date contractDateStart;
     private int maxKM;
     private int startOdometer;
-    private int customerIDContract;
-    private String plateNumberContract;
+    private int customerID;
+    private String plateNumber;
+
+
+
 }
