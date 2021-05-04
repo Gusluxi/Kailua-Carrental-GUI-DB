5112475a8a81f0c4e681943a55153c863a7f2f7c
FrederikALP <fred05k3@stud.kea.dk>
2021-05-04 11:04:13 +0200

    Zipcodes

--- src/main/java/com/example/demo/Repository/ZipcodeRepo.java
+++ src/main/java/com/example/demo/Repository/ZipcodeRepo.java
@@ -1,4 +1,24 @@
 package com.example.demo.Repository;
 
+import com.example.demo.model.Customer;
+import com.example.demo.model.Zipcode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class ZipcodeRepo {
+
+    @Autowired
+    JdbcTemplate template;
+
+    public List<Zipcode> fetchAll() {
+        String sql = "SELECT * FROM zipcodes";
+        RowMapper<Zipcode> rowMapper = new BeanPropertyRowMapper<>(Zipcode.class);
+        return template.query(sql, rowMapper);
+    }
 }
